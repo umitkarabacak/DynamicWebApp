ba2df3a091fd89c45030de28c66365602c98324e
Ümit KARABACAK <umit.karabacak@hotmail.com.tr>
2024-08-11 22:11:36 +0300

    Add project files.

---
+++ DynamicWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynamicWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
